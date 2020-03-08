a642498a2f2110e0f2dcf6b65f714c2651c6a86e
Chris Rehagen <ccrwy5@mail.missouri.edu>
2020-03-08 12:30:56 -0500

    First commit (test)

---
+++ Text-Trade/Text-Trade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Text-Trade
+//
+//  Created by Chris Rehagen on 3/8/20.
+//  Copyright © 2020 Chris Rehagen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("this is a test")
+    }
+
+
+}
+
