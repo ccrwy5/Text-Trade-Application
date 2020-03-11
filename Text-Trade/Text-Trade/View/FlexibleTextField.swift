84600f79600609e0113f026e9589d747bcb18bc7
Steven Perrin <spcnn@mail.missouri.edu>
2020-03-11 18:13:39 -0500

    adjusted text placeholder

---
+++ Text-Trade/Text-Trade/View/FlexibleTextField.swift
@@ -0,0 +1,21 @@
+//
+//  FlexibleTextField.swift
+//  Text-Trade
+//
+//  Created by Steven Perrin on 3/11/20.
+//  Copyright © 2020 Chris Rehagen. All rights reserved.
+//
+
+import UIKit
+
+class FlexibleTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
